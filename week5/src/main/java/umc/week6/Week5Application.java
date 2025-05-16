420a82a11cf8ecbb38e75a9e769711ad14db2051
bbandm <hjerachel@gmail.com>
2025-05-16 13:17:05 +0900

    week7

--- week5/src/main/java/umc/week6/Week5Application.java
+++ week5/src/main/java/umc/week6/Week5Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Week6Application {
+public class Week5Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Week6Application.class, args);
+		SpringApplication.run(Week5Application.class, args);
 	}
 
 }
